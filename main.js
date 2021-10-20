32e73e4b2a93cb8f628fae5dc15c66807196d4f9
Heisarki <heisarkiphawa@gmail.com>
2021-10-20 09:03:02 +0530

    V1

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const button = document.querySelector(".btn");
-const nav = document.querySelector('.menu')
+const nav_list = document.querySelector(".nav-list");
+const nav = document.querySelector('.nav');
+
+const header_width = document.querySelector(".header");
 
 let toggle = true;
 
-
+/*
 button.addEventListener('click', e => {
     console.log(toggle);
     
@@ -16,6 +18,29 @@ button.addEventListener('click', e => {
         nav.style.display = "inline";
         toggle = true;
     }
+})
+
+*/
+
+window.addEventListener('resize', e => {
+    console.log(window.innerHeight);
+    console.log(window.innerWidth)
+    let w = window.innerWidth;
+    let h = window.innerWidth;
+
+    if (w <= 700) {
+        nav.style.justifyContent = "center";
+        nav_list.style.display = "none";
+
+        header_width.style.height = "400px"
+    }
+    else {
+        nav_list.style.display = "flex"
+        nav.style.justifyContent = "space-between"
+
+        header_width.style.height = "600px"
+    }
+
 })
 
 
